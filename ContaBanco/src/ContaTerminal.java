beb2406ee696c22bef5d93a4ac1f27093d20def6
Isaque <andradeisq1.7@gmail.com>
2024-08-21 00:09:28 -0300

    modificacao metodos

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da Conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + conta +  " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
