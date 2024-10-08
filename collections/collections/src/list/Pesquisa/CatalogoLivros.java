5363832e851c9508f9d7d17b739deb959e51c1e2
Isaque <andradeisq1.7@gmail.com>
2024-10-08 02:18:51 -0300

    pesquisa usando list

---
+++ collections/collections/src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,53 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoInicial) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
---
+++ collections/collections/src/list/Pesquisa/SomaNumeros.java
@@ -0,0 +1,81 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+  //atributos
+  private List<Integer> numeros;
+
+  //construtor
+  public SomaNumeros() {
+    this.numeros = new ArrayList<>();
+  }
+
+  public void adicionarNumero(int numero) {
+    this.numeros.add(numero);
+  }
+
+  public int calcularSoma() {
+    int soma = 0;
+    for (Integer numero : numeros)
+      soma += numero;
+    return soma;
+  }
+
+  public int encontrarMaiorNumero() {
+    int maiorNumero = Integer.MIN_VALUE;
+    if (!numeros.isEmpty()) {
+      for (Integer numero : numeros) {
+        if (numero >= maiorNumero) {
+          maiorNumero = numero;
+        }
+      }
+      return maiorNumero;
+    } else {
+      throw new RuntimeException("A lista está vazia!");
+    }
+  }
+
+  public int encontrarMenorNumero() {
+    int menorNumero = Integer.MAX_VALUE;
+    if (!numeros.isEmpty()) {
+      for (Integer numero : numeros) {
+        if (numero <= menorNumero) {
+          menorNumero = numero;
+        }
+      }
+      return menorNumero;
+    } else {
+      throw new RuntimeException("A lista está vazia!");
+    }
+  }
+
+  public void exibirNumeros() {
+    if (!numeros.isEmpty()) {
+      System.out.println(this.numeros);
+    } else {
+      System.out.println("A lista está vazia!");
+    }
+  }
+
+  public static void main(String[] args) {
+    SomaNumeros somaNumeros = new SomaNumeros();
+
+    somaNumeros.adicionarNumero(5);
+    somaNumeros.adicionarNumero(0);
+    somaNumeros.adicionarNumero(0);
+    somaNumeros.adicionarNumero(-2);
+    somaNumeros.adicionarNumero(10);
+
+    System.out.println("Números adicionados:");
+    somaNumeros.exibirNumeros();
+
+    System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+    System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+    System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+  }
+}
+
