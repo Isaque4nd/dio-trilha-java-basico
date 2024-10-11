fc900ea9b9c2600cdb1aabcbc4b6d4fe37f0dbb2
Isaque <andradeisq1.7@gmail.com>
2024-10-11 02:18:01 -0300

    desafio 3, filtrando numeros positivos. utilizado filter para saber quais são maiores que 0 e collect para gerar nova lista

---
+++ StreamAPI/interfaces/src/StreamAPI/interfaces/src/Desafios/Desafio3.java
@@ -0,0 +1,21 @@
+package StreamAPI.interfaces.src.Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio3 {
+    
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, -10, -5, -4, -3);
+
+        List<Integer> numerosPositivos = numeros.stream()
+            .filter(numero -> numero > 0) 
+            .collect(Collectors.toList());
+
+        System.out.println(numerosPositivos);
+    
+    }
+
+}
