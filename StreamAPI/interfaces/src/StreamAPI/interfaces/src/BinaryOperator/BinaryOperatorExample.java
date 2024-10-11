f30753cdf7c69bb01026dceda3ecd7185ec8f876
Isaque <andradeisq1.7@gmail.com>
2024-10-11 01:41:11 -0300

    implementando o BinaryOperator

--- StreamAPI/interfaces/src/StreamAPI/interfaces/src/BinaryOperator/BinaryOperatorExample.java
+++ StreamAPI/interfaces/src/StreamAPI/interfaces/src/BinaryOperator/BinaryOperatorExample.java
@@ -1,5 +1,18 @@
 package StreamAPI.interfaces.src.BinaryOperator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BinaryOperatorExample {
     
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        //BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+
+        int resultado = numeros.stream()
+            .reduce(0,  Integer::sum);
+
+        System.out.println("A soma dos números é: " + resultado);
+    }
 }
