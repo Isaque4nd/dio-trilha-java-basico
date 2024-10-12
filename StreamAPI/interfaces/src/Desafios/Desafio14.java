393b78cf6e83d0c426ea119f3f73ce63023b89dd
Isaque <andradeisq1.7@gmail.com>
2024-10-12 02:38:26 -0300

    implementacao do desafio 14 para identificar qual o maior numero primo da lista

---
+++ StreamAPI/interfaces/src/Desafios/Desafio14.java
@@ -0,0 +1,22 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class Desafio14 {
+    
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        
+        Optional<Integer> maiorPrimo = numeros.stream()
+            .filter(n -> n > 1 && 
+                         IntStream.range(2, (int) Math.sqrt(n) + 1).noneMatch(i -> n % i == 0))
+            .max(Integer::compareTo);
+
+        maiorPrimo.ifPresent(primo -> System.out.println("O maior número primo é: " + primo));
+
+    }
+}
