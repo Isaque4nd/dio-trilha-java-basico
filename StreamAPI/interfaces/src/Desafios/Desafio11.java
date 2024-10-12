03f1953304590e6a3cd0f53b14c4129a52bbdef1
Isaque <andradeisq1.7@gmail.com>
2024-10-12 02:10:39 -0300

    implementacao do desafio 11, soma de todos os quadrados dos numeros da lista

---
+++ StreamAPI/interfaces/src/Desafios/Desafio11.java
@@ -0,0 +1,19 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio11 {
+    
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        
+        int somaDosQuadrados = numeros.stream()
+            .map(n -> n * n)
+            .reduce(0, Integer::sum);
+
+            System.out.println("A soma dos quadrados é: " + somaDosQuadrados);
+        
+    }
+}
