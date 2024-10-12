65cbe89a0f7eaad30c8f222869cc35c958f1a363
Isaque <andradeisq1.7@gmail.com>
2024-10-12 00:58:53 -0300

    implementacao do desafio 4 para imprimir apenas os números pares e o desafio 5 para imprimir a média dos numeros maiores que 5

---
+++ StreamAPI/interfaces/src/Desafios/Desafio4.java
@@ -0,0 +1,22 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio4 {
+    
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, -10, -5, -4, -3);
+
+        List<Integer> numerosImpares = numeros.stream()
+            .filter(n -> n % 2 == 0)
+            .collect(Collectors.toList());
+            
+
+        System.out.println(numerosImpares);
+        
+    }
+
+}
---
+++ StreamAPI/interfaces/src/Desafios/Desafio5.java
@@ -0,0 +1,20 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio5 {
+    
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, -10, -5, -4, -3);
+
+        double media = numeros.stream()
+            .filter(numero -> numero > 5) 
+            .mapToInt(Integer::intValue)
+            .average()
+            .orElse(0);
+
+            System.out.println("A soma dos números maiores que 5 é: " + media);
+    }
+}
